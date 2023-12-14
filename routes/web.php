7787c164113a42f7be0b95f958f354276d850d8e
Ken Waribo <degee147@gmail.com>
2023-12-14 10:24:00 +0100

    implemented admin auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +18,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::middleware(['auth:sanctum,admin', config('jetstream.auth_session'), 'verified',])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('dashboard');
+    })->name('admin_dashboard');
+});
+Route::middleware(['admin:admin'])->group(function () {
+    Route::get('/admin/login', [AdminController::class, 'loginForm'])->name('admin.login.form');
+    Route::post('/admin/login', [AdminController::class, 'store'])->name('admin.login');
+});
+
